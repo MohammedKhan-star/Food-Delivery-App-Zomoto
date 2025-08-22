51e4c0c301c18162efceb05e1a94147169e06275
Mohammed Khan <mohammedkhan20019@gmail.com>
2025-08-22 23:56:53 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder=async(req,res)=>{
 
-const frontend_url="http://localhost:3000"
+const frontend_url="https://food-delivery-app-zomoto-frontend1.onrender.com"
 
     try {
         const newOrder=new orderModel({
