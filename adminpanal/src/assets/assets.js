591637fa80dc4456bc93f93957e3bfb0c38fb512
Mohammed Khan <mohammedkhan20019@gmail.com>
2025-08-23 01:12:32 +0530

    Update assets.js

--- adminpanal/src/assets/assets.js
+++ adminpanal/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-app-zomoto-backend.onrender.com'
+export const url = 'https://food-delivery-app-zomoto-backend3.onrender.com'
