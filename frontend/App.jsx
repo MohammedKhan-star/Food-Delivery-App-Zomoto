802092e6e081a01b44dcc6be10b92903ad33159b
Mohammed Khan <your-github-email@gmail.com>
2026-01-02 14:14:59 +0530

    Fix item list

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -7,12 +7,15 @@ import PlaceOrder from './src/pages/PlaceOrder/PlaceOrder'
 import LoginPopup from './src/components/LoginPopup/LoginPopup'
 import Cart from './src/pages/Cart/Cart'
 import Verify from './src/pages/Verify/Verify';
-import MyOrders from './src/pages/MyOrders/MyOrders'
+import MyOrders from './src/pages/MyOrders/MyOrders';
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
+
 
 const App = () => {
   const [showLogin,setShowLogin]=useState(false)
   return (
     <>
+    
     {showLogin ? <LoginPopup setShowLogin={setShowLogin} />:<></>}
     <div className="app">
       <Navbar setShowLogin={setShowLogin} />
